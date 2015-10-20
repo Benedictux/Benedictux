da246ba7ea02c7dd0f78e985b03948945c1df6f9
B3n3dictux <b3n3dictux@gmail.com>
2015-10-20 22:22:32 +0200

    Commit Initiale du projet Benedictux

---
+++ README.md
@@ -0,0 +1,4 @@
+Benedictux
+==========
+
+A Symfony project created on October 19, 2015, 11:44 am.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,26 @@
+<?php
+//src/AppBundle/Controller/DefaultController.php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        // $posts = $this->getDoctrine()
+        //     ->getRepository('AppBundle:Post')
+        //     ->findLatest();
+
+        //return $this->render('default/index.html.twig', array(
+        //   'posts' => $posts
+        //));
+        return $this->render('default/index.html.twig');
+    }
+}
