32855f193578648f0eebaa7814210ac9eb84a103
Benedictux <b3n3dictux@gmail.com>
2015-10-21 18:02:58 +0200

    MAJ_entity_Post

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,9 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+//Need for createAction
+use AppBundle\Entity\Post;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -14,13 +17,37 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        // $posts = $this->getDoctrine()
-        //     ->getRepository('AppBundle:Post')
-        //     ->findLatest();
+        $posts = $this->getDoctrine()
+            ->getRepository('AppBundle:Post')
+            ->findAll();
+            //->findLatest();
 
-        //return $this->render('default/index.html.twig', array(
-        //   'posts' => $posts
-        //));
-        return $this->render('default/index.html.twig');
+        return $this->render('default/index.html.twig', array(
+          'posts' => $posts
+        ));
+    }
+
+    /**
+     * @Route("/create", name="createPost")
+     */
+    public function createAction()
+    {
+        $post = new Post();
+
+        //if ($form->isSubmitted() && $form->isValid()) {
+        $post->setTitle('Mon premier post');
+
+        $slug = $this->get('app.slugger')->slugify($post->getTitle());
+
+        $post->setSlug($slug);
+        $post->setContent('Voici officiellement le premier post d/inauguration du site Benedictux.com');
+        $post->setAuthorEmail('B3n3dictux@gmail.com');
+
+        $em = $this->getDoctrine()->getManager();
+
+        $em->persist($post);
+        $em->flush();
+
+        return new Response('Created post id '.$post->getId());
     }
 }
--- src/AppBundle/Entity/Post.php
+++ src/AppBundle/Entity/Post.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @ORM\Entity
+ * @ORM\Table(name="post")
  */
 class Post
 {
@@ -21,7 +22,7 @@ class Post
     private $id;
 
     /**
-     * @ORM\Column(type="string")
+     * @ORM\Column(type="string", length=200)
      */
     private $title;
 
@@ -45,21 +46,151 @@ class Post
      */
     private $publishedAt;
 
-    /**
-     * @ORM\OneToMany(
-     *      targetEntity="Comment",
-     *      mappedBy="post",
-     *      orphanRemoval=true
-     * )
-     * @ORM\OrderBy({"publishedAt" = "ASC"})
-     */
-    private $comments;
+   // /**
+   //  * @ORM\OneToMany(
+   //  *      targetEntity="Comment",
+   //  *      mappedBy="post",
+   //  *      orphanRemoval=true
+   //  * )
+   //  * @ORM\OrderBy({"publishedAt" = "ASC"})
+   //  */
+   // private $comments;
 
     public function __construct()
     {
         $this->publishedAt = new \DateTime();
-        $this->comments = new ArrayCollection();
+        //$this->comments = new ArrayCollection();
     }
 
     // getters and setters ...
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Post
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     *
+     * @return Post
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return Post
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set authorEmail
+     *
+     * @param string $authorEmail
+     *
+     * @return Post
+     */
+    public function setAuthorEmail($authorEmail)
+    {
+        $this->authorEmail = $authorEmail;
+
+        return $this;
+    }
+
+    /**
+     * Get authorEmail
+     *
+     * @return string
+     */
+    public function getAuthorEmail()
+    {
+        return $this->authorEmail;
+    }
+
+    /**
+     * Set publishedAt
+     *
+     * @param \DateTime $publishedAt
+     *
+     * @return Post
+     */
+    public function setPublishedAt($publishedAt)
+    {
+        $this->publishedAt = $publishedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get publishedAt
+     *
+     * @return \DateTime
+     */
+    public function getPublishedAt()
+    {
+        return $this->publishedAt;
+    }
 }
