6e55447957797774d2d93f8cd171393f369326e7
Benedictux <b3n3dictux@gmail.com>
2015-10-23 10:17:03 +0200

    Ajout_de_createAction_showAction_et_updateAction_dans_DefaultController_du_AppBundle

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -28,7 +28,17 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/create", name="createPost")
+     * @Route("/{slug}", name="show_post")
+     */
+    public function showAction(Post $post)
+    {
+        return $this->render('default/show.html.twig', array(
+          'post' => $post
+          ));
+    }
+
+    /**
+     * @Route("/create", name="create_post")
      */
     public function createAction()
     {
@@ -38,8 +48,8 @@ class DefaultController extends Controller
         $post->setTitle('Mon premier post');
 
         $slug = $this->get('app.slugger')->slugify($post->getTitle());
-
         $post->setSlug($slug);
+
         $post->setContent('Voici officiellement le premier post d/inauguration du site Benedictux.com');
         $post->setAuthorEmail('B3n3dictux@gmail.com');
 
@@ -49,5 +59,28 @@ class DefaultController extends Controller
         $em->flush();
 
         return new Response('Created post id '.$post->getId());
+    }
+
+    /**
+     * @Route("/update/{id}", name="update_post")
+     */
+    public function updateAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $post = $em->getRepository('AppBundle:Post')->find($id);
+
+        if (!$post) {
+            throw $this->createNotFoundException(
+                'No post found for id '.$id
+            );
+        }
+
+        $post->setTitle('New post name!');
+        $slug = $this->get('app.slugger')->slugify($post->getTitle());
+        $post->setSlug($slug);
+
+        $em->flush();
+
+        return $this->redirectToRoute('homepage');
     }
 }
